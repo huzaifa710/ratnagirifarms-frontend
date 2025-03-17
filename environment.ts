d892964bc5f21f41ecc26026d738ac8887cd69fb
huzaifa710 <58044615+huzaifa710@users.noreply.github.com>
2025-03-17 16:41:00 +0530

    Update environment.ts

--- environment.ts
+++ environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  // API_URL: "https://api.ratnagirifarms.com/staging",
-  API_URL: "http://localhost:3001",
+  API_URL: "https://api.ratnagirifarms.com/staging",
+  // API_URL: "http://localhost:3001",
 };
