b09de617d1e5621cf83e4a1cb25d8ac4b92480e0
huzaifa710 <58044615+huzaifa710@users.noreply.github.com>
2025-03-21 17:48:08 +0530

    sitemap changes

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://ratnagirifarms.com',   // Replace with your domain
+    generateRobotsTxt: true,             // Automatically generate robots.txt
+    sitemapSize: 7000,
+  };
