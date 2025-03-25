a646ae015144b86dc6b0f5c20c9089d0d9c8a7f4
huzaifa710 <58044615+huzaifa710@users.noreply.github.com>
2025-03-25 16:15:41 +0530

    T&C changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,64 @@
-import Image from "next/image";
-import HomePage from "@/app/home/page"
+import JsonLd from '@/app/component/JsonLd';
+import HomePage from "@/app/home/page";
+import Script from "next/script";
 
 export default function Home() {
-  return(<HomePage />)
-  
+  return (
+    <>
+      <Script
+        id="homepage-schema"
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify({
+            "@context": "https://schema.org",
+            "@type": "WebSite",
+            name: "Ratnagiri Farms",
+            url: "https://ratnagirifarms.com",
+            potentialAction: {
+              "@type": "SearchAction",
+              target:
+                "https://ratnagirifarms.com/search?q={search_term_string}",
+              "query-input": "required name=search_term_string",
+            },
+          }),
+        }}
+        strategy="afterInteractive"
+      />
+
+      <Script
+        id="organization-schema"
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify({
+            "@context": "https://schema.org",
+            "@type": "Organization",
+            name: "Ratnagiri Farms",
+            url: "https://ratnagirifarms.com",
+            logo: "https://ratnagirifarms.com/home/ratnagiri1.png",
+            description:
+              "Premium Alphonso mangoes direct from Ratnagiri orchards",
+            address: {
+              "@type": "PostalAddress",
+              addressLocality: "Bankot, Tal. Mandangad",
+              addressRegion: "Ratnagiri, Maharashtra",
+              postalCode: "415208",
+              addressCountry: "IN",
+            },
+            contactPoint: {
+              "@type": "ContactPoint",
+              email: "support@ratnagirifarms.com",
+              contactType: "customer service",
+            },
+            sameAs: [
+              "https://www.instagram.com/ratnagirifarms",
+              // "https://www.facebook.com/ratnagirifarms",
+            ],
+          }),
+        }}
+        strategy="afterInteractive"
+      />
+
+      <HomePage />
+    </>
+  );
 }
