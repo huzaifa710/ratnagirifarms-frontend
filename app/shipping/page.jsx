5f7b4f48b97d92645238d2ee95533d872d9153bd
huzaifa710 <58044615+huzaifa710@users.noreply.github.com>
2025-04-06 22:36:25 +0530

    location changes

--- app/shipping/page.jsx
+++ app/shipping/page.jsx
@@ -18,7 +18,7 @@ export default function ShippingPolicy() {
 
         <div className={styles.section}>
           <h2>Shipping Locations</h2>
-          <p>Delivering all over India.</p>
+          <p>Delivering all over Maharashtra.</p>
         </div>
 
         <div className={styles.section}>
