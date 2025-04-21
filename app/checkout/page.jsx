d866e750a6b6390b354f4997f32fa8b8b98cdcb4
huzaifa710 <58044615+huzaifa710@users.noreply.github.com>
2025-04-21 12:01:31 +0530

    added new states

--- app/checkout/page.jsx
+++ app/checkout/page.jsx
@@ -451,8 +451,8 @@ export default function Checkout() {
                 <option value="Chhattisgarh">Chhattisgarh</option> */}
                 <option value="Goa">Goa</option>
                 <option value="Gujarat">Gujarat</option>
-                {/* <option value="Haryana">Haryana</option>
-                <option value="Himachal Pradesh">Himachal Pradesh</option>
+                <option value="Haryana">Haryana</option>
+                {/*<option value="Himachal Pradesh">Himachal Pradesh</option>
                 <option value="Jharkhand">Jharkhand</option>*/}
                 <option value="Karnataka">Karnataka</option>
                 {/* <option value="Kerala">Kerala</option>  */}
@@ -468,10 +468,10 @@ export default function Checkout() {
                 <option value="Sikkim">Sikkim</option>
                 <option value="Tamil Nadu">Tamil Nadu</option>
                 <option value="Telangana">Telangana</option>
-                <option value="Tripura">Tripura</option>
+                <option value="Tripura">Tripura</option>*/}
                 <option value="Uttar Pradesh">Uttar Pradesh</option>
                 <option value="Uttarakhand">Uttarakhand</option>
-                <option value="West Bengal">West Bengal</option>
+                {/*<option value="West Bengal">West Bengal</option>
                 <option value="Andaman and Nicobar Islands">Andaman and Nicobar Islands</option>
                 <option value="Chandigarh">Chandigarh</option>
                 <option value="Dadra and Nagar Haveli and Daman and Diu">Dadra and Nagar Haveli and Daman and Diu</option> */}
