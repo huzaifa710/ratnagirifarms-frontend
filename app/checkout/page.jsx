3b7f41f76ac422bdc3c84954533ca2386bb850bf
Huzaifa <huzaifa@gyanberry.com>
2025-07-10 15:21:56 +0530

    Update page.jsx

--- app/checkout/page.jsx
+++ app/checkout/page.jsx
@@ -486,9 +486,9 @@ export default function Checkout() {
                 required
               >
                 <option value="">Select State</option>
-                {/*<option value="Andhra Pradesh">Andhra Pradesh</option>
-                 <option value="Arunachal Pradesh">Arunachal Pradesh</option>
-                <option value="Assam">Assam</option>*/}
+                <option value="Andhra Pradesh">Andhra Pradesh</option>
+                <option value="Arunachal Pradesh">Arunachal Pradesh</option>
+                <option value="Assam">Assam</option>
                 <option value="Bihar">Bihar</option>
                 <option value="Chandigarh">Chandigarh</option>
                 <option value="Chhattisgarh">Chhattisgarh</option>
@@ -496,7 +496,7 @@ export default function Checkout() {
                 <option value="Goa">Goa</option>
                 <option value="Gujarat">Gujarat</option>
                 <option value="Haryana">Haryana</option>
-                {/*<option value="Himachal Pradesh">Himachal Pradesh</option>*/}
+                <option value="Himachal Pradesh">Himachal Pradesh</option>
                 <option value="Jharkhand">Jharkhand</option>
                 <option value="Karnataka">Karnataka</option>
                 <option value="Kerala">Kerala</option>
@@ -509,19 +509,23 @@ export default function Checkout() {
                 <option value="Uttar Pradesh">Uttar Pradesh</option>
                 <option value="Uttarakhand">Uttarakhand</option>
                 <option value="West Bengal">West Bengal</option>
-                {/* <option value="Manipur">Manipur</option>
+                <option value="Manipur">Manipur</option>
                 <option value="Meghalaya">Meghalaya</option>
                 <option value="Mizoram">Mizoram</option>
                 <option value="Nagaland">Nagaland</option>
                 <option value="Odisha">Odisha</option>
-                <option value="Sikkim">Sikkim</option>*/}
+                <option value="Sikkim">Sikkim</option>
                 <option value="Tamil Nadu">Tamil Nadu</option>
-                {/*  <option value="Tripura">Tripura</option>
-                <option value="Andaman and Nicobar Islands">Andaman and Nicobar Islands</option>
-                <option value="Dadra and Nagar Haveli and Daman and Diu">Dadra and Nagar Haveli and Daman and Diu</option>
+                <option value="Tripura">Tripura</option>
+                <option value="Andaman and Nicobar Islands">
+                  Andaman and Nicobar Islands
+                </option>
+                <option value="Dadra and Nagar Haveli and Daman and Diu">
+                  Dadra and Nagar Haveli and Daman and Diu
+                </option>
                 <option value="Jammu and Kashmir">Jammu and Kashmir</option>
                 <option value="Ladakh">Ladakh</option>
-                <option value="Lakshadweep">Lakshadweep</option>*/}
+                <option value="Lakshadweep">Lakshadweep</option>
               </select>
               <div className={styles.inputGroup}>
                 <input
